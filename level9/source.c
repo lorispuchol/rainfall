ea977f1cef50d5f55cac2cc160c474e9cec3583c
Loris Puchol <lorispuchol@gmail.com>
2024-08-26 12:43:11 -0400

    source level9

---
+++ level9/source.c
@@ -0,0 +1,30 @@
+
+/* N::N(int) */
+
+void __thiscall N::N(N *this,undefined4 param_1)
+
+{
+  *(undefined ***)this = &PTR_operator+_08048848;
+  *(undefined4 *)(this + 0x68) = param_1;
+  return;
+}
+
+
+void main(int param_1,int param_2)
+
+{
+  N *this;
+  undefined4 *this_00;
+  
+  if (param_1 < 2) {
+                    /* WARNING: Subroutine does not return */
+    _exit(1);
+  }
+  this = (N *)operator.new(0x6c);
+  N::N(this,5);
+  this_00 = (undefined4 *)operator.new(0x6c);
+  N::N((N *)this_00,6);
+  N::setAnnotation(this,*(char **)(param_2 + 4));
+  (**(code **)*this_00)(this_00,this);
+  return;
+}
